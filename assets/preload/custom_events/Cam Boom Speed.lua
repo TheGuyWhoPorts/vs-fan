0597d3c32b19cb0189f4f3fa521b439d38e2a480
TheGuyWhoPorts <121072594+TheGuyWhoPorts@users.noreply.github.com>
2022-12-21 20:13:02 -0500

    Add files via upload

---
+++ assets/preload/custom_events/Cam Boom Speed.lua
@@ -0,0 +1,20 @@
+boomspeed = 4
+bam = 1
+function onEvent(n,v1,v2)
+
+if n == "Cam Boom Speed" then
+
+boomspeed = tonumber(v1)
+bam = tonumber(v2)
+
+end
+
+end
+
+function onBeatHit()
+
+	if curBeat % boomspeed == 0 then
+		triggerEvent("Add Camera Zoom",0.01*bam,0.02*bam)
+	end
+
+end
